66864264c30666acf5f9573cc8b01979660793e7
andrewbelles <andrewbelles31@gmail.com>
2025-06-13 16:59:26 -0400

    Better Visualization of Boundaries

--- topo-preference/display_state.py
+++ topo-preference/display_state.py
@@ -1,30 +1,45 @@
 import sqlite3
-import pandas as pd 
-from shapely import wkt
-from shapely import geometry
+import pandas as pd
+import geopandas as gpd
 import matplotlib.pyplot as plt
+from shapely import wkt
 
-attom = sqlite3.connect("attom_data.db")
-df = pd.read_sql_query("SELECT bound_wkt FROM state_TN_boundary LIMIT 1;", attom)
-attom.close()
+attom_db = sqlite3.connect("attom_data.db")
 
-geom = wkt.loads(df.loc[0, 'bound_wkt'])
+# 2. Loop through your state tables and build one DataFrame
+frames = []
+states = [
+    "AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",
+    "HI","ID","IL","IN","IA","KS","KY","LA","ME","MD",
+    "MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ",
+    "NM","NY","NC","ND","OH","OK","OR","PA","RI","SC",
+    "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
+]
 
-plt.figure()
-polys = []
-if isinstance(geom, geometry.MultiPolygon):
-    polys = list(geom.geoms)
-elif isinstance(geom, geometry.Polygon):
-    polys = [geom]
-else: 
-    raise ValueError(f"Unexpected Geometry Type: {geom.geom_type}")
+# Get each wkt boundary as a geometry and append to frame list 
+for state in states:
+    table = f"state_{state}_boundary"
+    try:
+        df = pd.read_sql_query(f"SELECT bound_wkt FROM {table};", attom_db)
+    except Exception:
+        continue
+    df["code"] = state
+    df["geometry"] = df["bound_wkt"].apply(wkt.loads)
+    frames.append(df[["code","geometry"]])
 
-for poly in polys:
-    x, y = poly.exterior.xy
-    plt.plot(x, y)
+attom_db.close()
 
-plt.title("Boundary of TN")
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.axis("equal")
+# Geopandas stuff to get the projection to look correct
+gdf = gpd.GeoDataFrame(pd.concat(frames, ignore_index=True),
+                       geometry="geometry",
+                       crs="EPSG:4326")
+
+# Plot projected frames 
+gdf_us = gdf.to_crs(epsg=5070)
+fig, ax = plt.subplots()
+gdf_us.boundary.plot(ax=ax)
+
+ax.set_title("50 States Boundary Maps")
+ax.set_axis_off()
+plt.tight_layout()
 plt.show()
